4a134cdd464398165d474df444e1bb8d3a1cb571
Crowdbotics <team@crowdbotics.com>
2024-05-23 10:18:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-hall-48081",
+  url: "https://rough-hall-48081.botics.co",
+  api: "https://rough-hall-48081.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
